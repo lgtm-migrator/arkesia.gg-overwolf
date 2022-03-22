4669792a4325b2ac054a32922f843606c1916d8f
lmachens <leon@machens.koeln>
2022-03-22 14:53:25 +0100

    ✨ Display hotkey in app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import { useRunningGameInfo } from "../lib/games";
+import { useHotkeyBinding } from "../lib/hotkeys";
 import useWindowInfo from "../lib/useWindowInfo";
 import {
   closeMainWindow,
@@ -16,11 +17,23 @@ import iconSrc from "./icon.png";
 function AppHeader() {
   const windowInfo = useWindowInfo();
   const runningGameInfo = useRunningGameInfo();
+  const showHideAppHotkeyBinding = useHotkeyBinding("show_hide_app");
 
   return (
     <header className={styles.header} onMouseDown={dragMoveWindow}>
-      <img src={iconSrc} alt="" className={styles.icon} />
-      <h1 className={styles.title}>Arkesia.gg</h1>
+      <div className={styles.flex}>
+        <img src={iconSrc} alt="" className={styles.icon} />
+        <h1 className={styles.title}>Arkesia.gg</h1>
+      </div>
+      <div className={styles.hotkey}>
+        Show/Hide{" "}
+        <a
+          className={styles.chip}
+          href="overwolf://settings/games-overlay?hotkey=show_hide_app&gameId=21864"
+        >
+          {showHideAppHotkeyBinding.toUpperCase()}
+        </a>
+      </div>
       <div className={styles.actions}>
         <button
           className={styles.action}
---
+++ src/lib/useHotkeyBinding.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { LOST_ARK_CLASS_ID } from "./games";
+
+export function useHotkeyBinding(name: string): string {
+  const [hotkeyBinding, setHotkeyBinding] = useState<string>("");
+
+  useEffect(() => {
+    overwolf.settings.hotkeys.get((result) => {
+      if (result.games) {
+        const hotkey = result.games[LOST_ARK_CLASS_ID].find(
+          (hotkey) => hotkey.name === name
+        );
+        if (hotkey) {
+          setHotkeyBinding(hotkey.binding);
+        }
+      }
+    });
+
+    const handleChange = (event: overwolf.settings.hotkeys.OnChangedEvent) => {
+      if (event.name === name) {
+        setHotkeyBinding(event.binding);
+      }
+    };
+    overwolf.settings.hotkeys.onChanged.addListener(handleChange);
+
+    return () => {
+      overwolf.settings.hotkeys.onChanged.removeListener(handleChange);
+    };
+  }, []);
+
+  return hotkeyBinding;
+}
