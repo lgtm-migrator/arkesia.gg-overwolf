4cac7367c96158f7ee2eefb4b35f5cfc0c427c61
Leon Machens <leon@machens.koeln>
2022-06-01 12:14:15 +0200

    Revert "✨ Add ads"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import AppHeader from "./components/AppHeader";
 import styles from "./App.module.css";
 import ResizeBorders from "./components/ResizeBorders";
-import Ads from "./components/Ads";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <ResizeBorders />
       <iframe className={styles.iframe} src={import.meta.env.VITE_APP_WEB} />
       <div className={styles.loading}>Loading map...</div>
-      <Ads />
     </div>
   );
 }
